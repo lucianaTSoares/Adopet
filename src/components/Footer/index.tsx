bd5bb3d404366d4782fbd89f977808551f4d49ac
Luciana <luciana.soares@6place.com.br>
2022-09-16 23:30:54 -0300

    style: remove unnecessary tailwind css classes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Text } from "../Text";
 
 export default function Footer() {
   return (
-    <div className="h-20 bg-primary flex justify-center items-center text-white relative">
+    <div className="h-20 bg-primary flex justify-center items-center text-white">
       <Text>Feito com amor por Luciana</Text>
     </div>
   );
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { HomeIcon, MailIcon, LogoWhite } from "../../utils/exportSVG";
 
 export default function Navbar() {
   return (
-    <nav className="absolute h-32 w-screen flex  items-center">
+    <nav className="absolute h-32 w-screen flex items-center">
       <div className="flex gap-12 px-12">
         <span className="hidden sm:block">
           <LogoWhite width={128} height={30} />
