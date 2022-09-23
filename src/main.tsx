b018398326bd4ce5e7dd48c54fd0b5b3cd174946
Luciana <luciana.soares@6place.com.br>
2022-09-23 17:45:30 -0300

    refactor: removes Background component from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Background>
-        <App />
-      </Background>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
