e98a41404fd4bf70b11045cba9e15b68d5eedec4
Luciana <luciana.soares@6place.com.br>
2022-10-04 16:27:12 -0300

    feat: code of login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,8 @@
+import { useContext, useEffect } from "react";
+import logoBlue from "../../assets/logo/adopet-color.svg";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { Text } from "../../components/Text";
-import logoBlue from "../../assets/logo/adopet-color.svg";
-import { useContext, useEffect } from "react";
 import { PagesContext } from "../../context/PagesContext";
 
 export default function Login() {
@@ -19,27 +19,16 @@ export default function Login() {
   );
 
   return (
-    <div className="mx-6 h-screen flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center items-center mt-7">
-        <img src={logoBlue} className="w-40" />
+    <div className="mx-6 h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center absolute top-40">
+        <img src={logoBlue} className="w-40 mb-6" alt="" />
         <Text color="blue">
-          Ainda não tem cadastro?
-          Então, antes de buscar seu melhor amigo, precisamos de alguns dados:
+          Já tem conta?{" "}
+          <span className="font-semibold">Faça seu login agora!</span>
         </Text>
-        <Input
-          label="Email"
-          id="email"
-          placeholder="Escolha seu melhor email"
-          type="email"
-        />
-        <Input label="Nome" id="nome" placeholder="Digite seu nome completo" />
-        <Input label="Senha" id="password" placeholder="Crie uma senha" />
-        <Input
-          label="Confirme sua senha"
-          id="passwordConfirmation"
-          placeholder="Repita a senha criada acima"
-        />
-        <Button margin={"1.5rem 0 0 0"}>Cadastrar</Button>
+        <Input label="Email" id="email" placeholder="Insira seu email" />
+        <Input label="Senha" id="password" placeholder="Insira sua senha" />
+        <Button margin={"1.5rem 0 0 0"}>Entrar</Button>
       </div>
     </div>
   );
